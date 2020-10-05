2b97817e3d994a534361c2d7b479d23d3849e293
zemiguel20 <migli.coutinho@gmail.com>
2020-10-05 01:28:36 +0100

    a1-database removed

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,6 @@ import Body from './Body';
 import Database from '../backend/Database';
 
 class App extends Component {
-
-    constructor(props) {
-        super(props);
-        this.db = new Database();
-    }
-
     render() {
         return (
             <Body></Body>
